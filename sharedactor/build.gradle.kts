9e08c9ec1893f6eeff5f9c9385c4ba3cbbc137b4
John Winters <john.daniel.winters@gmail.com>
2024-06-22 19:36:47 -0700

    main - incr build no

--- sharedactor/build.gradle.kts
+++ sharedactor/build.gradle.kts
@@ -91,7 +91,7 @@ val org.jetbrains.kotlin.konan.target.KonanTarget.archVariant: String
         "ios-arm64_armv7"
     }
 
-val v = "1.2.101"
+val v = "1.2.102"
 group = "com.exoteric"
 version = v
 
